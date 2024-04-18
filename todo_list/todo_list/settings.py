8727cf3d5215090475521355b129a5d111774316
pratyush6653 <pratyush0231@gmail.com>
2024-04-18 21:34:47 +0530

    fourth commit

--- todo_list/todo_list/settings.py
+++ todo_list/todo_list/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^mi-o9f4@y5@omi(k7f9$lpxaqqp$%p3e#a!u5tq)eetc#*lh_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app','.now.sh']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -127,6 +127,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
-
+# STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+#
