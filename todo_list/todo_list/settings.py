b6d9e6ab1b47f48b104fadd99851c91f280bfce5
Pratyush6653 <94372256+pratyush6653@users.noreply.github.com>
2024-04-20 10:52:46 +0530

    Update settings.py

--- todo_list/todo_list/settings.py
+++ todo_list/todo_list/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'django_todolist',
         'USER': 'root',
-        'PASSWORD': 'pratyush6653@',
+        'PASSWORD': '*******',
         'HOST': 'localhost',
         'PORT': '3306',
     }
